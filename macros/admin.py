eeb8ba1a1a88a730fcfb978328d238a89b38ec82
Jordan Ribera <jribera@rover.com>
2016-03-10 15:33:07 -0800

    show shortcut in admin list

--- macros/admin.py
+++ macros/admin.py
@@ -4,7 +4,7 @@ from .models import UnicodeMacro
 
 
 class UnicodeMacroAdmin(admin.ModelAdmin):
-    list_display = ('safe_text',)
+    list_display = ('safe_text', 'shortcut')
 
     class Media:
         css = {
